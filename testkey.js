6f09e3ea0149443d66c1d83376e544b9353a3743
Vitor <vitorbiliato2@gmail.com>
2025-02-23 12:39:00 -0300

    Add files via upload

---
+++ testkey.js
@@ -0,0 +1,18 @@
+const key = "earlyTinyVirtual022025_cardboard"
+let access
+
+function verifyAccess() {
+    if(localStorage.getItem("keyValue") != key) {
+        access = prompt("Please enter key for early access below:")
+    }
+    if (access != key) {
+        verifyAccess()
+    } else  localStorage.setItem("keyValue", key);
+}
+console.warn("The error below is not an error and is a security feature, just ignore cuz works as intended")
+
+if (Math.random() <= 0.15){
+    let e = new RangeError("Uncaught Index at line 82: \"Never gonna give you up, Never gonna let you...\"")
+    console.error(e)
+}
+verifyAccess()
