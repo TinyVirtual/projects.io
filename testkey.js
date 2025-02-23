dffe4ee31742dea13435ce1c08725f7d77193703
Vitor <vitorbiliato2@gmail.com>
2025-02-23 12:55:11 -0300

    Update testkey.js

--- testkey.js
+++ testkey.js
@@ -9,7 +9,7 @@ function verifyAccess() {
         verifyAccess()
     } else localStorage.setItem("keyValue", key);
 }
-console.warn("The error below is not an error and is a security feature, just ignore cuz works as intended")
+//console.warn("The error below is not an error and is a security feature, just ignore cuz works as intended")
 
 if (Math.random() <= 0.15){
     let e = new RangeError("Uncaught Index at line 82: \"Never gonna give you up, Never gonna let you...\"")
